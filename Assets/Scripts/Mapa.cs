8102f3091229541ec05cb0a315373e3a7ae73dc5
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2022-01-22 15:25:56 -0300

    Mapa de Grama

--- Assets/Scripts/Mapa.cs
+++ Assets/Scripts/Mapa.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class Mapa : MonoBehaviour
 {
-    [SerializeField] public Vector2 mapSize;
+    [SerializeField] public Vector2Int mapSize;
+    [SerializeField] public Vector2Int grassPerAxis;
+    [SerializeField] public GameObject grassObject;
     void Start()
     {
-        
+        grassPerAxis = new Vector2Int(mapSize.x/20, mapSize.y/20);
+        Vector3 pointer = new Vector3(-mapSize.x / 2f + 10f, 0f, -mapSize.y / 2f + 10f);
+        for(int x = 0; x < grassPerAxis.x; x++)
+        {
+            for (int y = 0; y < grassPerAxis.y; y++)
+            {
+                GameObject grass = Instantiate(grassObject, GameObject.Find("Grasses").transform);
+                grass.transform.position = pointer;
+                pointer += Vector3.back * -20;
+            }
+            pointer = new Vector3(pointer.x, 0f, -mapSize.y / 2f + 10f);
+            pointer += Vector3.right * 20;
+        }
     }
 
-    void Update()
+    void OnDrawGizmos()
     {
-        
+        Gizmos.DrawWireCube(transform.position, new Vector3(mapSize.x, 1, mapSize.y));
     }
 }
