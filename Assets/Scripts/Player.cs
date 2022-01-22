8bef32039fb338f614e0cfcef1fe199a85f91152
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2022-01-22 18:11:44 -0300

    Mudanca

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,29 +1,79 @@
 using UnityEngine;
-using System.Collections;
 
-public class Player : MonoBehaviour {
-	public float movementSpeed = 10;
-	public float turningSpeed = 60;
-	public int force = 1;
-	public Arvore arvore;
+[RequireComponent(typeof(CharacterController))]
+
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+    public int force = 1;
+    public Arvore arvore;
 
 
-	void Update() {
-		float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-		transform.Rotate(0, horizontal, 0);
-		
-		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-		transform.Translate(0, 0, vertical);
+    public float speed = 7.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
 
-		baterUma();
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 rotation = Vector2.zero;
 
-	}
+    [HideInInspector]
+    public bool canMove = true;
 
-	void baterUma()
+    void Start()
     {
-		if (Input.GetKey("space"))
+        characterController = GetComponent<CharacterController>();
+        rotation.y = transform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
         {
-			arvore.vida = arvore.vida -1;
+            // We are grounded, so recalculate move direction based on axes
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            if (Input.GetButton("Jump") && canMove)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player and Camera rotation
+        if (canMove)
+        {
+            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+            playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+            transform.eulerAngles = new Vector2(0, rotation.y);
+        }
+
+        baterUma();
+    }
+
+    void baterUma()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            arvore.vida = arvore.vida - 1;
         }
     }
 }
