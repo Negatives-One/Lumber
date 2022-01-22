e61f3425f84e44bff33b1ead0f6e356641a9775c
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2022-01-22 14:38:25 -0300

    Setup

---
+++ Assets/Scripts/Mapa.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapa : MonoBehaviour
+{
+    [SerializeField] public Vector2 mapSize;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
