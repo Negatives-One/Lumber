6647014880f57574ec7b20347c7b5d016202c874
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2022-01-22 18:04:10 -0300

    Spawn arvores

--- Assets/Scripts/Mapa.cs
+++ Assets/Scripts/Mapa.cs
@@ -7,6 +7,10 @@ public class Mapa : MonoBehaviour
     [SerializeField] public Vector2Int mapSize;
     private Vector2Int grassPerAxis;
     [SerializeField] public GameObject grassObject;
+    [SerializeField] public List<GameObject> grasses = new List<GameObject>();
+
+    [SerializeField] private List<GameObject> arvores = new List<GameObject>();
+
     void Start()
     {
         grassPerAxis = new Vector2Int(mapSize.x/20, mapSize.y/20);
@@ -16,11 +20,17 @@ public class Mapa : MonoBehaviour
             for (int y = 0; y < grassPerAxis.y; y++)
             {
                 GameObject grass = Instantiate(grassObject, GameObject.Find("Grasses").transform);
+                grasses.Add(grass);
                 grass.transform.position = pointer;
                 pointer += Vector3.back * -20;
             }
             pointer = new Vector3(pointer.x, 0f, -mapSize.y / 2f + 10f);
             pointer += Vector3.right * 20;
+        }
+
+        for(int i = 0; i < grasses.Count; i++)
+        {
+            Instantiate(arvores[Random.Range(0, 2)], grasses[i].transform.position, Quaternion.identity, GameObject.Find("Arvores").transform);
         }
     }
 
